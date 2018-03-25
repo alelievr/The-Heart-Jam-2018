1971be37e1edd95b3f3fbe69d646f1f89aa0c080
Gabriel FOURNIER <gabfou95@gmail.com>
2018-03-25 11:12:40 +0200

    Fgdsg

--- Assets/Scripts/OumphController.cs
+++ Assets/Scripts/OumphController.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class OumphController : MonoBehaviour {
-=======
+
 public class OmphController : MonoBehaviour {
->>>>>>> 5b2877c2f94019268334964a00119681d901ed71
 
 
 	public GameObject	arm;
