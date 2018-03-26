0127caa6bd84052a8fbf9769cee1f982f36675ec
Gabriel FOURNIER <gabfou95@gmail.com>
2018-03-26 05:14:02 +0200

    dsf

---
+++ Assets/soundintro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundintro : MonoBehaviour {
+
+	// Use this for initialization
+	int step = 0;
+	public List<AudioClip>	soundbystep;
+	AudioSource				aus = null;
+
+	void Start () {
+		aus = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnEnable()
+	{
+		if (aus == null)
+			aus = GetComponent<AudioSource>();
+		aus.PlayOneShot(soundbystep[step]);
+		step++;
+	}
+}
